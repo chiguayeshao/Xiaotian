04d08153d8d59211fb6001737c57d6c64baba969
gem <gem@cian.app>
2022-04-20 21:31:50 +0800

    update navbar css

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -2,18 +2,32 @@ const Navbar = () => {
     return (
         <div className="bg-slate-400">
             <nav className="relative container mx-auto">
-                <div className="flex justify-between items-center">
-                    <div className="m-5">
-                        <a href="/" className=" font-bold text-white">
+                <div className="flex justify-center items-center">
+                    <div className="m-5 pr-20">
+                        <a href="/" className=" font-bold text-white text-2xl">
                             Xiaotian Ye
                         </a>
                     </div>
-                    <a href="/categories">Categories</a>
-                    <a href="/tags">Tags</a>
-                    <a href="/archive">Archive</a>
-                    <a href="/apps">Apps</a>
-                    <a href="/about">About</a>
-                    <a href="/search">Search</a>
+                    <div className="hidden md:flex space-x-6">
+                        <a href="/categories" className="text-white">
+                            Categories
+                        </a>
+                        <a href="/tags" className="text-white">
+                            Tags
+                        </a>
+                        <a href="/archive" className="text-white">
+                            Archive
+                        </a>
+                        <a href="/apps" className="text-white">
+                            Apps
+                        </a>
+                        <a href="/about" className="text-white">
+                            About
+                        </a>
+                        <a href="/search" className="text-white">
+                            Search
+                        </a>
+                    </div>
                 </div>
             </nav>
         </div>
